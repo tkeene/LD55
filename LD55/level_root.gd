6032d24b20f4b959aa04d3dec3d9d01c3ebafff0
@koboldskeep <eneekmot@yahoo.com>
2024-04-13 16:42:53 -0600

    Fixed Control
    * Fixed the controls being unintuitive, made them better fit Action/Place/Cancel actions.

--- LD55/level_root.gd
+++ LD55/level_root.gd
@@ -25,20 +25,22 @@ func _ready():
 func _process(delta):
 	var unscaled_delta = 1.0 / 60.0
 	music_player.volume_db = move_toward(music_player.volume_db, 0.0, FADE_IN_SPEED * unscaled_delta )
-	if Input.is_action_pressed("ui_cancel"):
-		quit_held_time += unscaled_delta
-		if quit_held_time > 1.0:
-			get_tree().change_scene_to_file("res://01_main_menu/01_main.tscn")
-	else:
-		quit_held_time = 0.0
-	if Input.is_action_just_pressed("Toggle"):
-		if Engine.time_scale != 0.0:
-			pause_game()
+	if Engine.time_scale != 0.0:
+		($OverlayUI/SelectedScrollRect as CanvasItem).visible = false
+		if Input.is_action_pressed("ui_cancel"):
+			quit_held_time += unscaled_delta
+			if quit_held_time > 1.5:
+				get_tree().change_scene_to_file("res://01_main_menu/01_main.tscn")
 		else:
+			quit_held_time = 0.0
+		if Input.is_action_just_pressed("Toggle"):
+			pause_game()
+	else:
+		# Object Placement UI
+		quit_held_time = 0.0
+		if Input.is_action_just_pressed("ui_cancel"):
 			un_pause_game()
-	# Object Placement UI
-	if Engine.time_scale == 0.0:
-		if current_placing_object == null:
+		elif current_placing_object == null:
 			($OverlayUI/SelectedScrollRect as CanvasItem).visible = true
 			if placeable_objects.size() > 0:
 				if Input.is_action_just_pressed("ui_right"):
@@ -51,12 +53,14 @@ func _process(delta):
 					current_spellbook_index = 0
 				var selected_object : PackedScene = placeable_objects[current_spellbook_index]
 				($OverlayUI/SelectedScrollRect/SelectedScrollLabel as RichTextLabel).text = "[color=black]" + selected_object.resource_path
-				if Input.is_action_just_pressed("ui_accept"):
+				if Input.is_action_just_pressed("ui_accept") ||  Input.is_action_just_pressed("Toggle"):
 					current_placing_object = selected_object.instantiate() as Node2D
 					current_placing_object.global_position = Vector2(500, 400)
 					add_child(current_placing_object)
 			else:
 				($OverlayUI/SelectedScrollRect/SelectedScrollLabel as RichTextLabel).text = "[color=black]You have no more pages!"
+				if Input.is_action_just_pressed("ui_accept") ||  Input.is_action_just_pressed("Toggle"):
+					un_pause_game()
 		else:
 			($OverlayUI/SelectedScrollRect as CanvasItem).visible = false
 			if Input.is_action_pressed("ui_left"):
@@ -67,14 +71,10 @@ func _process(delta):
 				current_placing_object.global_position += Vector2.UP * MOVE_PLACING_OBJECT_SPEED * unscaled_delta
 			if Input.is_action_pressed("ui_down"):
 				current_placing_object.global_position += Vector2.DOWN * MOVE_PLACING_OBJECT_SPEED * unscaled_delta
-			
-			if Input.is_action_just_pressed("ui_accept"):
+			if Input.is_action_just_pressed("ui_accept") ||  Input.is_action_just_pressed("Toggle"):
 				placeable_objects.remove_at(current_spellbook_index)
 				current_placing_object = null;
 				un_pause_game()
-	else:
-		($OverlayUI/SelectedScrollRect as CanvasItem).visible = false
-
 
 func un_pause_game():
 	if current_placing_object != null:
@@ -84,7 +84,7 @@ func un_pause_game():
 	music_player.stream = load(NORMAL_MUSIC)
 	music_player.volume_db = FADE_IN_VOLUME
 	music_player.play()
-	$OverlayUI/ControlsPromptRect/ControlsPromptLabel.text = "[color=black]C (hold 1 second) - Quit\nX - Summon\nZ - Jump"
+	$OverlayUI/ControlsPromptRect/ControlsPromptLabel.text = "[color=black]C (hold 1 second) - Reset\nX - Summon\nZ - Jump"
 
 func pause_game():
 	current_placing_object = null
@@ -92,4 +92,4 @@ func pause_game():
 	music_player.stream = load(PAUSED_MUSIC)
 	music_player.volume_db = FADE_IN_VOLUME
 	music_player.play()
-	$OverlayUI/ControlsPromptRect/ControlsPromptLabel.text = "[color=black]C (hold 1 second) Quit\nX - Return To Level\n◀/▶ Select/Move\nZ - Place"
+	$OverlayUI/ControlsPromptRect/ControlsPromptLabel.text = "[color=black]C - Return\nX/Z - Place\n◀/▶ Select/Move"

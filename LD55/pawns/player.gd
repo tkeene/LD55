ed8041417f6f9d07e9ca6378ad49d652622727a9
@koboldskeep <eneekmot@yahoo.com>
2024-04-13 12:51:11 -0600

    Player Basic Controller
    Player can move and jump

--- LD55/pawns/player.gd
+++ LD55/pawns/player.gd
@@ -2,15 +2,18 @@ extends CharacterBody2D
 
 const GROUND_MAX_SPEED : float = 4000.0
 const GROUND_ACCELERATION : float = 500.0
-const GRAVITY : float = 100.0
+const GRAVITY : float = 200.0
 const DEFAULT_JUMP_COOLDOWN : float = 0.25
-const JUMP_VELOCITY : float = 80.0
+const JUMP_VELOCITY : float = 120.0
+const GHOST_JUMP_DURATION = 0.30
 const DECELERATION : float = 0.1
 
 var remaining_jump_cooldown_seconds = 0.0
+var remaining_floor_time = 0.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	
 	pass # Replace with function body.
 
 func _process(delta):
@@ -20,6 +23,10 @@ func _process(delta):
 func _physics_process(delta):
 	if delta > 0.0:
 		remaining_jump_cooldown_seconds -= delta
+		if is_on_floor():
+			remaining_floor_time = GHOST_JUMP_DURATION
+		else:
+			remaining_floor_time -= delta
 		# Input
 		var horizontal_input : float = 0.0
 		var jump_pressed : bool = false
@@ -27,12 +34,9 @@ func _physics_process(delta):
 			horizontal_input -= 1.0
 		if Input.is_action_pressed("Right"):
 			horizontal_input += 1.0
-		if Input.is_action_just_pressed("Action") and remaining_jump_cooldown_seconds <= 0.0:
+		if (Input.is_action_just_pressed("Action") and remaining_jump_cooldown_seconds <= 0.0
+			and remaining_floor_time >= 0.0):
 			jump_pressed = true
-		# Floor Check
-		# TODO physics check
-		# TODO coyote time
-		var is_on_floor = true
 		# Platforming Physics
 		var current_velocity : Vector2 = velocity
 		current_velocity.x = horizontal_input * GROUND_MAX_SPEED * delta

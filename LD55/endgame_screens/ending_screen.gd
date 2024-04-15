b966fc1e765cbd6b61bc3fdd65678cb0dac97bb1
@koboldskeep <eneekmot@yahoo.com>
2024-04-14 22:09:09 -0600

    Added Endings
    * Added placeholder ending spells for 3 different endings

---
+++ LD55/endgame_screens/ending_screen.gd
@@ -0,0 +1,13 @@
+extends Node
+
+var restart_timer = 20.0
+
+func _ready():
+	Engine.time_scale = 1.0
+
+func _process(delta):
+	restart_timer -= delta
+	if Input.is_action_just_pressed("ui_cancel"):
+		restart_timer *= 0.05
+	if restart_timer <= 0:
+		get_tree().change_scene_to_file("res://01_main_menu/01_main.tscn")
